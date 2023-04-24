4689a886a7874984da1e1118fa7fc302e1988bb9
Alice <achan520@terpmail.umd.edu>
2023-04-23 21:14:25 -0400

    Chart display

--- script.js
+++ script.js
@@ -6,38 +6,110 @@ Then make sure that your data is visible through your chosen visualization libra
  // Callback that creates and populates a data table,
       // instantiates the pie chart, passes in the data and
       // draws it.
+// Load the Visualization API and the corechart package.
+google.charts.load('current', {'packages':['corechart']});
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(drawChart);
+
 function drawChart() {
 
     // Create the data table.
-    const data = new google.visualization.DataTable();
-    data.addColumn('string', 'Topping');
-    data.addColumn('number', 'Slices');
-    data.addRows([
-        ['Mushrooms', 3],
-        ['Onions', 1],
-        ['Olives', 1],
-        ['Zucchini', 1],
-        ['Pepperoni', 2]
+   const data = new google.visualization.DataTable();
+   data.addColumn('string', 'Dog Breed');
+   data.addColumn('number', 'Number for Each Breed');
+   data.addRows([
+        ['Affenpinscher', 1],
+        ['African', 1],
+        ['Airedale', 1],
+        ['Akita', 1],
+        ['Appenzeller', 1],
+        ['Australian', 3]
     ]);
+    //const info = Object.keys(object).map((item) => object[item].length);
 
     // Set chart options
-    const options = {'title':'How Many Dogs in Each Dog Breed',
+    const options = {'title':'The Percent of Dogs That Make Up Each Breed',
     'width':400,
     'height':300};
 
 // Instantiate and draw our chart, passing in some options.
-    const chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    const chart = new google.visualization.PieChart(document.getElementById('chart'));
     chart.draw(data, options);
 }
 
+
+
+//Take an array and return an object
+function shapeData(array){
+    return array.reduce((collection, item) => {
+
+        //If there's no key then create new array with the first thing that matches that key
+        if(!collection[item.category]) {
+            collection[item.category] = [item]
+
+        }
+
+        //If exists, then push the item currently looking at into that key to make a new array of every thing in the list
+        else{
+            collection[item.category].push(item);
+        }
+
+        return collection;
+    }, {});
+    
+}
+
+async function getData(){
+    const url = 'https://dog.ceo/api/breeds/list/all';
+    const data = await fetch(url);
+    const json = await data.json();
+    const reply = json.filter((item) => Boolean(item.geocoded_column_1)).filter((item) => Boolean(item.name));
+    return reply;
+}
+
 async function mainEvent() {
+    
     const mainForm = document.querySelector('.main_form');
+    const loadDataButton = document.querySelector('#data_load');
+    const clearDataButton = document.querySelector('#data_clear');
+    const generateListButton = document.querySelector('#generate');
+
+    const loadAnimation = document.querySelector('#data_load_animation');
+    const chartTarget = document.querySelector('#chart');
+
+    loadAnimation.style.display = 'none';
+    generateListButton.classList.add('hidden');
 
 
-    const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const results = await fetch('https://dog.ceo/api/breeds/list/all');
+    const arrayFromJson = await results.json();
 
-    /*textField.addEventListener('input', (event) => {
 
-    });*/
+    //API Data Request
+    const chartData = await getData();
+    const shapedData = shapeData(chartData);
+    console.log(shapedData);
+    const myChart = drawChart(chartTarget, shapeData);
+
+    if (chartData.length > 0){
+        submit.style.display = 'block';
+        loadAnimation.classList.remove('lds-ellipsis');
+        loadAnimation.classList.add('lds-ellipsis-hidden');
+
+        let currentArray;
+        mainForm.addEventListener('submit', async (SubmitEvent) => {
+            SubmitEvent.preventDefault();
+            currentArray = processBreeds(chartData);
+
+            const dogs = currentArray.filter((item) => Boolean(item.geocoded_column_1));
+
+            injectHTML(dogs);
+
+
+        });
+
+    }
+
 }
 
